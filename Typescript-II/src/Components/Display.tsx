33b1e5c521b392d53aa6e8e61f0a97428d61fcc0
Muskan Shaw <princesahil0065@gmail.com>
2022-05-20 12:12:11 +0530

    day 2

--- Typescript-II/src/Components/Display.tsx
+++ Typescript-II/src/Components/Display.tsx
@@ -1,8 +1,64 @@
-import React from 'react'
-
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import style from "../Styles/Form.module.css"
+type formdata ={
+  id:number,
+  price:number;
+  height:number;
+  width:number;
+  make_year:number;
+  model:string;
+  op_system:string;
+}
 const Display = () => {
+  const [data,setData]=useState<formdata[]>([])
+  const get =()=>{
+    axios.get("http://localhost:8080/formData").then((res:AxiosResponse<formdata[]>)=>{
+     const  {data}=res
+    //  console.log(data)
+     setData(data)
+    })
+  }
+  useEffect(()=>{
+    get()
+    console.log(data)
+  },[])
   return (
-    <div>Display</div>
+    <div>
+      <h3>See Details Here</h3>
+      <table className={style.table}>
+        <thead>
+          <tr>
+            <td>Sl. No.</td>
+            <td>Id</td>
+            <td>Model</td>
+            <td>Make year</td>
+            <td>Operating System</td>
+            <td>Screen weigth</td>
+            <td>Screen Width</td>
+            <td>Price</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((e,i)=>{
+              return (
+                <tr key={i}>
+                  <th>{i+1}.</th>
+                  <th>{e.id}</th>
+                  <th>{e.model}</th>
+                  <th>{e.make_year}</th>
+                  <th>{e.op_system}</th>
+                  <th>{e.height}</th>
+                  <th>{e.width}</th>
+                  <th>{e.price}</th>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- Typescript-II/src/Components/Form.tsx
+++ Typescript-II/src/Components/Form.tsx
@@ -1,36 +1,47 @@
+import axios from "axios";
 import { useState } from "react"
 import style from "../Styles/Form.module.css"
 const Form = () => {
+  
     const [formData,setFormData]=useState({})
-    const handleChange=(e)=>{
+    const handleChange=(e:any)=>{
         let input=e.target.name;
-         
-    }
+        setFormData({...formData,
+        [input]:e.target.value
+        })
+         }
+  const handleSubmit=(e:any)=>{
+    e.preventDefault();
+    console.log(formData)
+    axios.post("http://localhost:8080/formData",formData)
+  }
   return (
     <div className={style.form}>
         <h4>Fill your Laptop's details Here</h4>
-        <label htmlFor="">Enter Id No.</label>
-        <input type="id" />
+      <form action="" onSubmit={handleSubmit}>
+      <label htmlFor="">Enter Id No.</label>
+        <input type="id"  name="id" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Model</label>
-        <input type="text" />
+        <input type="text" name="model" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Make Year</label>
-        <input type="year" />
+        <input type="year" name="make_year" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Operating System</label>
-        <input type="text" />
+        <input type="text" name="op_system" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Screen Height</label>
-        <input type="height" />
+        <input type="height" name="height" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Screen WIdth</label>
-        <input type="width" />
+        <input type="width" name="width" onChange={handleChange}/>
         <br />
         <label htmlFor="">Enter Price in Rs.</label>
-        <input type="price" />
+        <input type="price" name="price" onChange={handleChange}/>
         <br />
         <input type="submit" />
+      </form>
     </div>
   )
 }
