19f303635a0611e656bfc8ef890f3b4e92bf95fd
Muskan Shaw <princesahil0065@gmail.com>
2022-05-25 15:36:03 +0530

    done

--- custom-hooks/src/Components/Merge.tsx
+++ custom-hooks/src/Components/Merge.tsx
@@ -1,23 +1,23 @@
-import React, { SyntheticEvent } from 'react'
-import useMergeState from '../Hooks/UseMergeState'
+import {SyntheticEvent} from 'react';
+import useMergeState from '../Hooks/UseMarge';
 
-const Merge = () => {
-  const state  = useMergeState() 
+function Merge() {
+
+const state  = useMergeState() 
   const handleSubmit = (e:SyntheticEvent)=>{
     e.preventDefault()
     console.log(state)
   }
   return (
-    <div>
-           <form onSubmit={handleSubmit} >
+    <div className="App">
+     <form onSubmit={handleSubmit} >
        <input name='username' type="text" placeholder='Enter Username' />
        <input name='email' type="email" placeholder='Enter Email' />
        <input name='password' type="password" placeholder='Enter Password' />
        <input type="submit" value="submit" />
      </form>
-
     </div>
-  )
+  );
 }
 
-export default Merge
+export default Merge;
