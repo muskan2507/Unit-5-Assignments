25a779a49b5648677d4f93725ee69ed474e4ec45
Muskan Shaw <princesahil0065@gmail.com>
2022-06-05 16:42:05 +0530

    progress

--- Express-Middlewares/File Uploader/index.js
+++ Express-Middlewares/File Uploader/index.js
@@ -1,6 +1,8 @@
 let fs=require("fs");
 let express=require("express")
 const app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.get("/",(req,res)=>{
@@ -14,6 +16,27 @@ app.get("/",(req,res)=>{
     })
 
 })
+app.post('/', upload.single('avatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+    console.log(req)
+    // req.body will hold the text fields, if there were any
+  })
+  
+  app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
+  
+  const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+  app.post('/cool-profile', cpUpload, function (req, res, next) {
+    // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+    //
+    // e.g.
+    //  req.files['avatar'][0] -> File
+    //  req.files['gallery'] -> Array
+    //
+    // req.body will contain the text fields, if there were any
+  })
 app.listen(8080,()=>{
     console.log("Server started at port http://localhost:8080/*")
 })
