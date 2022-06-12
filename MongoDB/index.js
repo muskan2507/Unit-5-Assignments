4c5695c381c840fa3961f3d11fd36ababd1c59a1
Muskan Shaw <princesahil0065@gmail.com>
2022-06-12 09:20:09 +0530

    done

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -4,18 +4,40 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.get("/movies",async(req,res)=>{
-    const movies = await Movie.find();
-    return res.json(movies);
+    const {search,sort,pageNo} = req.query;
+    if(search===undefined && sort===undefined && pageNo===undefined){
+        const movies = await Movie.find();
+        return res.json(movies);
+    }
+    else if(sortBy){
+        const movis = await Movie.find().sort({[sort]: 1}) ;
+        return res.json(movies);
+    }
+    else if(search){
+        const movies = await Movie.find({ title: { $regex: search } });
+        return res.json(movies);
+    }
+    else if(page){
+        if(page===1){
+            const movies = await Movie.find().limit(5);
+            return res.json(movies);
+        }
+        else{
+            const show = 5*(page-1);
+            const movies = await Movie.find().skip(show).limit(5);
+            return res.json(movies);
+        }
+    }
 })
-app.get("/movies/:title",async(req,res)=>{
-  const {title}=req.params;
-  const movies=await Movie.find({title:title})
-  return res.json(movies)
-})
+
 app.post("/movies",(req,res)=>{
     const movie = new Movie({...req.body});
     movie.save((err,movie)=>{
-        res.json(movie);
+        try {
+            res.send(movie);
+        } catch {
+            res.send(err);
+        }
     })
 })
 
